aed6cd8ad0a6523c9fd39aa6393c646aad21990f
yhj0329 <wlrnfhd0329@gmail.com>
2023-03-27 19:47:45 +0900

    feat: 시작화면과 설정화면 연결, 마우스를 만나면 자동선택 수정해야함

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
 import sys
+import pickle
 import in_game
 from button import Button
 from option import basic_option as basic
 from option.setting_option import Option
 
+try:
+    with open("./option/save_option.pickle", "rb") as f:
+        display_size = pickle.load(f)
+        color_weakness = pickle.load(f)
+        key_setting = pickle.load(f)
+except EOFError:
+    display_size = basic.display_size
+    color_weakness = basic.color_weakness
+    key_setting = basic.key_setting
+
+if display_size[0] == 1920:
+    font_size = basic.font_size[0]
+    button_size = basic.button_size[0]
+elif display_size[0] == 1600:
+    font_size = basic.font_size[1]
+    button_size = basic.button_size[1]
+else:
+    font_size = basic.font_size[2]
+    button_size = basic.button_size[2]
 
 # Pygame 초기화
 pygame.init()
@@ -16,7 +36,6 @@ def settings_button_click_event():
     print('설정 버튼 클릭됨')
     option = Option()
     option.run()
-    del option
 
 
 def exit_button_click_event():
@@ -26,22 +45,22 @@ def exit_button_click_event():
 
 
 # 화면 표시
-screen_width, screen_height = 1920, 1080
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("sUNOftware Engineering")
+screen = pygame.display.set_mode(display_size)
+pygame.display.set_caption(basic.game_title)
 
 # 로고 표시
 logo = pygame.image.load("resources/Image/logo.jpg")
 logo_rect = logo.get_rect()
-logo_rect.center = (screen_width // 2, logo_rect.height // 2)
+logo_rect.center = (display_size[0] // 2, logo_rect.height // 2)
 
 # 버튼
-buttons = [Button(screen_width // 2, screen_height // 2, 200, 75, '싱글 플레이', in_game.game),
-           Button(screen_width // 2, screen_height // 2 * 1.3, 200, 75, '설정', settings_button_click_event),
-           Button(screen_width // 2, screen_height // 2 * 1.6, 200, 75, '나가기', exit_button_click_event)]
+buttons = [Button(display_size[0] // 2, display_size[1] // 2, button_size[0], button_size[1], '싱글 플레이', in_game.game, font_size[1]),
+           Button(display_size[0] // 2, display_size[1] // 2 * 1.3, button_size[0], button_size[1], '설정', settings_button_click_event, font_size[1]),
+           Button(display_size[0] // 2, display_size[1] // 2 * 1.6, button_size[0], button_size[1], '나가기', exit_button_click_event, font_size[1])]
 
 selected_button_index = 0
 buttons[selected_button_index].selected = True
+
 
 # 배경 색상
 screen.fill((20, 20, 20))
@@ -72,7 +91,6 @@ while running:
 
     # 매 프레임마다 화면 업데이트
     pygame.display.flip()
-
 
 # 나가기
 pygame.quit()
