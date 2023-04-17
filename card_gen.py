f5d073dd5984b6f20422686be6e7928a626fcccd
YooHyeokJin <wlrnfyd0329@gmail.com>
2023-04-17 22:34:06 +0900

    feat:single_game 배경음악 추가, 카드 사이즈 변경

--- card_gen.py
+++ card_gen.py
@@ -1,20 +1,11 @@
 import pygame
+from option import basic_option as basic
 
 # 색 카드 추가를 위한 리스트
 colors = ['red', 'green', 'blue', 'yellow']
 values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'reverse', 'draw_2', 'bomb', 'one_more']
 change_colors = ['red', 'green', 'blue', 'yellow']
 change_value = None
-
-
-# 색약 모드 옵션을 위한 임시값 (추후 삭제)
-class Option:
-    def __init__(self, color_weakness):
-        self.color_weakness = color_weakness
-
-
-# Option 클래스를 인스턴스화합니다.
-option = Option(False)
 
 
 class Card:
@@ -34,9 +25,9 @@ class Card:
         return self.value in ["skip", "reverse", "draw_2", "bomb", "one_more", "shield", "change"]
 
 
-def generate_cards(color_weakness):
+def generate_cards(color_weakness, size_by):
     cards = []
-    card_back_image = pygame.image.load("resources/Image/card_images/card_back.png")
+    card_back_image = pygame.transform.scale_by(pygame.image.load("resources/Image/card_images/card_back.png"), size_by)
 
     card_folder = "resources/image/cw_card_images" if color_weakness else "resources/image/card_images"
     # 색약 모드와 경로 차별화
@@ -44,7 +35,7 @@ def generate_cards(color_weakness):
     for i in range(2):
         for color in colors:
             for value in values:
-                card_image = pygame.image.load(f"{card_folder}/{color}_{value}.png")
+                card_image = pygame.transform.scale_by(pygame.image.load(f"{card_folder}/{color}_{value}.png"), size_by)
                 card = Card(color, value, card_image, card_back_image)
                 cards.append(card)
                 if value == "bomb":
@@ -52,26 +43,26 @@ def generate_cards(color_weakness):
 
     # 색 없는 실드카드를 한 번에 추가합니다.
     for i in range(2):
-        card_image = pygame.image.load(f"resources/Image/card_images/none_shield.png")
+        card_image = pygame.transform.scale_by(pygame.image.load(f"resources/Image/card_images/none_shield.png"), size_by)
         card = Card('none', 'shield', card_image, card_back_image)
         cards.append(card)
 
     # 색 없는 색변경 카드를 추가
     for i in range(2):
-        card_image = pygame.image.load(f"resources/Image/card_images/none_change.png")
+        card_image = pygame.transform.scale_by(pygame.image.load(f"resources/Image/card_images/none_change.png"), size_by)
         card = Card('none', 'change', card_image, card_back_image)
         cards.append(card)
     return cards
 
 
 # 체인지카드 발동시 유저한테 보여질 카드.
-def generate_for_change_cards(color_weakness):
+def generate_for_change_cards(color_weakness, size_by):
     for_change_cards = []
     change_card_folder = "resources/image/cw_for_change_cards" if color_weakness else "resources/image/for_change_cards"
-    card_back_image = pygame.image.load("resources/Image/card_images/card_back.png")
+    card_back_image = pygame.transform.scale_by(pygame.image.load("resources/Image/card_images/card_back.png"), size_by)
 
     for color in change_colors:
-        card_image = pygame.image.load(f"{change_card_folder}/{color}_{change_value}.png")
+        card_image = pygame.transform.scale_by(pygame.image.load(f"{change_card_folder}/{color}_{change_value}.png"), size_by)
         for_change_card = Card(color, change_value, card_image, card_back_image)
         for_change_cards.append(for_change_card)
     return for_change_cards
